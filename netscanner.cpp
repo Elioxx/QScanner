3712b049e471d0980c9153e4e399832f55d8301d
elioxx <elias.schweighofer@gmail.com>
2022-01-20 18:39:38 +0000

    Added Class netscanner and added gui

---
+++ netscanner.cpp
@@ -0,0 +1,6 @@
+#include "netscanner.h"
+
+NetScanner::NetScanner()
+{
+
+}
---
+++ netscanner.h
@@ -0,0 +1,12 @@
+#ifndef NETSCANNER_H
+#define NETSCANNER_H
+
+#include <QObject>
+
+class NetScanner
+{
+public:
+    NetScanner();
+};
+
+#endif // NETSCANNER_H
